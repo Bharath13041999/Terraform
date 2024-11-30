a13f2f6cf9429c48b61e8f6bb5db59e1711b8892
Bharath13041999 <bharath.d.m@capgemini.com>
2024-11-30 07:05:13 +0000

    deployment

---
+++ main.tf
@@ -0,0 +1,53 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+ 
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${var.resource_group_name}-vnet"
+  location            = azurerm_resource_group.rg.location
+resource_group_name = azurerm_resource_group.rg.name
+address_space = ["10.0.0.0/16"]
+}
+ 
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.resource_group_name}-subnet"
+resource_group_name = azurerm_resource_group.rg.name
+virtual_network_name = azurerm_virtual_network.vnet.name
+address_prefixes = ["10.0.1.0/24"]
+}
+ 
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.vm_name}-nic"
+  location            = azurerm_resource_group.rg.location
+resource_group_name = azurerm_resource_group.rg.name
+ 
+  ip_configuration {
+    name                          = "internal"
+subnet_id = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+ 
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                  = var.vm_name
+resource_group_name = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = var.vm_size
+  admin_username        = var.admin_username
+  admin_password        = var.admin_password
+network_interface_ids = [azurerm_network_interface.nic.id]
+ 
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+ 
+  source_image_reference {
+    publisher = var.image_reference.publisher
+    offer     = var.image_reference.offer
+    sku       = var.image_reference.sku
+    version   = var.image_reference.version
+  }
+}
+

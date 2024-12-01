cf632a82bfe4106bd748d0196eaf9ddd959b3985
Bharath13041999 <bharath.d.m@capgemini.com>
2024-12-01 12:23:14 +0000

    aws EC2 deployment

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "vm_name" {
-  description = "The name of the VM"
-value = azurerm_windows_virtual_machine.vm.name
-}
- 
-output "vm_ip_address" {
-  description = "The private IP address of the VM"
-  value       = azurerm_network_interface.nic.private_ip_address
-}
